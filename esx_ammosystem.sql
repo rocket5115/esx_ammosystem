982eef5997c4a269b5e9117be6dcf7a5eb073a14
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-05-29 00:47:49 +0200

    Add files via upload

---
+++ esx_ammosystem.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('ammo', 'Pistol ammo', 250, 0, 1),
+('crb_ammo', 'Carabine ammo', 250, 0, 1),
+('sht_ammo', 'Shotgun ammo', 250, 0, 1),
+('smg_ammo', 'SMG ammo', 250, 0, 1),
+('snp_ammo', 'Sniper ammo', 250, 0, 1);
